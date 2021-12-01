4970a8053f9e553143e38ad6372d20636c556d0d
Jenny Fu <jenny.fu@stonybrook.edu>
2021-12-01 00:01:50 -0500

    title screen changes

--- client/src/components/SplashScreen.js
+++ client/src/components/SplashScreen.js
@@ -1,8 +1,27 @@
+import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';
+import SortIcon from '@mui/icons-material/Sort';
+import ConnectWithoutContactIcon from '@mui/icons-material/ConnectWithoutContact';
+
 export default function SplashScreen() {
     return (
         <div id="splash-screen">
-            The Top 5<br />
-            Lister
+            <div id='title'>Welcome to The Top 5 Lister!</div>
+            <div id='subtitle'>Created by Jenny Fu ft. McKilla Gorilla</div>
+            <div class='info-banner'>
+                <div class='banner-text'>Interact With User Owned Lists!</div>
+                <LocalLibraryIcon class='banner-icon'/>
+            </div>
+            <div class='info-banner'>
+                <div class='banner-text'>Create Your Own Lists!</div>
+                <SortIcon class='banner-icon'/>
+            </div>
+            <div class='info-banner'>
+                <div class='banner-text'>Build a Community!</div>
+                <ConnectWithoutContactIcon class='banner-icon'/>
+            </div>
+            <div class='title-buttons'>
+                
+            </div>
         </div>
     )
 }
